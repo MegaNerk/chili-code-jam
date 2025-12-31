d88949463ac14c6db03bd63347d79c90ea3bd90b
MegaNerk <odrecord@rcn.com>
2025-12-31 02:20:42 -0500

    Add: Time system architecture

--- In Game/Game Logic/game.gd
+++ In Game/Game Logic/game.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var event_director : EventDirector
 @export var city_director : CityDirector
+@export var time_coordinator : TimeCoordinator
 
 var active_kaiju : Array[Kaiju]
 var active_buildings : Array[Building]

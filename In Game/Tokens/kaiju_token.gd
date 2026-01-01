1c6ca3dce9d8e665d2039f5e12cfabbf5a675749
Jason clini <33248688+Psyceric@users.noreply.github.com>
2026-01-01 14:28:36 -0500

    Kaiju move at different speed on land / water

--- In Game/Tokens/kaiju_token.gd
+++ In Game/Tokens/kaiju_token.gd
@@ -11,7 +11,9 @@ signal unselected
 @export var pin_click_mask : Texture2D
 @export var nav_agent : NavigationAgent2D
 @export var char_body : CharacterBody2D
-var speed = 200
+var speed = 100
+var water_speed = 150
+var land_speed = 70
 var target_position = Vector2.ZERO
 var pin_click_mask_image : Image
 
@@ -26,16 +28,38 @@ func _ready():
 func _traveled_water_way(details):
 	print("Kaiju {name} used the {waterway}".format({"name" : char_body.name, "waterway" : details.rid}))
 
+func _physics_process(delta):
+	char_body.move_and_slide()
+
+func _update_speed():
+	match _get_current_region().navigation_layers:
+		1:
+			speed = water_speed
+		2:
+			speed = land_speed
+		_:
+			print("Not on Nav Mesh")
+
+
 func _update_location():
-	print("Test")
-	if nav_agent.is_target_reachable() and not nav_agent.is_navigation_finished():
-		print("Moving")
+	if nav_agent.is_navigation_finished():
+		char_body.velocity = Vector2.ZERO
+		return
+
+	_update_speed()
+	if nav_agent.is_target_reachable():
 		var next_point = nav_agent.get_next_path_position()
 		var direction = char_body.global_position.direction_to(next_point)
 		char_body.velocity = direction * speed
 	else:
 		char_body.velocity = Vector2.ZERO
-	char_body.move_and_slide()
+	
+func _get_current_region():
+	var cur_reg = NavigationServer2D.agent_get_map(nav_agent.get_rid())
+	var closest = NavigationServer2D.map_get_closest_point_owner(cur_reg, char_body.global_position)
+	var object_instance = instance_from_id(NavigationServer2D.region_get_owner_id(closest)) as NavigationRegion2D
+	return object_instance
+	
 
 func on_hovered():
 	self.scale = Vector2(1.1,1.1)

3c97fd99ea4270789e542554619b695409b77187
MegaNerk <odrecord@rcn.com>
2026-01-01 17:49:51 -0500

    Add: City Pin

--- In Game/Tokens/Kaiju/kaiju_token.gd
+++ In Game/Tokens/Kaiju/kaiju_token.gd
@@ -1,33 +1,22 @@
-extends Control
+extends TokenPin
 class_name KaijuToken
 
-signal hovered
-signal hover_stopped
-signal left_clicked
-signal right_clicked
-signal selected
-signal unselected
-
-@export var pin_click_mask : Texture2D
 @export var nav_agent : NavigationAgent2D
 @export var char_body : CharacterBody2D
 var speed = 100
 var water_speed = 150
 var land_speed = 70
 var target_position = Vector2.ZERO
-var pin_click_mask_image : Image
 var game : Game
 var time_cordinator : TimeCoordinator
-
-var currently_selected : bool = false
 
 var my_kaiju : Kaiju
 
 func _ready():
-	pin_click_mask_image = pin_click_mask.get_image()
+	super()
 	nav_agent.link_reached.connect(_traveled_water_way)
 	game = get_tree().current_scene as Game
-	
+
 func _traveled_water_way(details):
 	print("Kaiju {name} used the {waterway}".format({"name" : char_body.name, "waterway" : details.rid}))
 
@@ -55,52 +44,8 @@ func _physics_process(delta):
 		
 	char_body.move_and_slide()
 
-	
 func _get_current_region():
 	var cur_reg = NavigationServer2D.agent_get_map(nav_agent.get_rid())
 	var closest = NavigationServer2D.map_get_closest_point_owner(cur_reg, char_body.global_position)
 	var object_instance = instance_from_id(NavigationServer2D.region_get_owner_id(closest)) as NavigationRegion2D
 	return object_instance
-	
-
-func on_hovered():
-	self.scale = Vector2(1.1,1.1)
-	emit_signal("hovered")
-
-func on_stop_hovered():
-	if currently_selected == false:
-		self.scale = Vector2(1,1)
-	emit_signal("hover_stopped")
-
-func get_selected():
-	currently_selected = true
-	emit_signal("selected")
-
-func get_unselected():
-	currently_selected = false
-	on_stop_hovered()
-	emit_signal("unselected")
-
-func _gui_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			emit_signal("left_clicked")
-		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			emit_signal("right_clicked")
-
-func _has_point(point):
-	if pin_click_mask == null or pin_click_mask.get_size() == Vector2.ZERO:
-		return false
-	
-	var pin_click_mask_image = pin_click_mask.get_image()
-	var mask_size : Vector2 = pin_click_mask_image.get_size()
-	
-	var uv = point/size
-	var point_on_mask = (uv * mask_size).floor()
-	
-	if not Rect2(Vector2.ZERO, mask_size).has_point(point_on_mask):
-		return false
-	
-	var point_r_value = pin_click_mask_image.get_pixelv(point_on_mask).r
-	
-	return point_r_value < 0.5
---
+++ In Game/Tokens/token_pin.gd
@@ -0,0 +1,59 @@
+extends Control
+class_name TokenPin
+
+signal hovered
+signal hover_stopped
+signal left_clicked
+signal right_clicked
+signal selected
+signal unselected
+
+@export var pin_click_mask : Texture2D
+var pin_click_mask_image : Image
+
+var currently_selected : bool = false
+
+func _ready():
+	pin_click_mask_image = pin_click_mask.get_image()
+	
+func on_hovered():
+	self.scale = Vector2(1.1,1.1)
+	emit_signal("hovered")
+
+func on_stop_hovered():
+	if currently_selected == false:
+		self.scale = Vector2(1,1)
+	emit_signal("hover_stopped")
+
+func get_selected():
+	currently_selected = true
+	emit_signal("selected")
+
+func get_unselected():
+	currently_selected = false
+	on_stop_hovered()
+	emit_signal("unselected")
+
+func _gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			emit_signal("left_clicked")
+		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			emit_signal("right_clicked")
+
+func _has_point(point):
+	if pin_click_mask == null or pin_click_mask.get_size() == Vector2.ZERO:
+		return false
+	
+	var pin_click_mask_image = pin_click_mask.get_image()
+	var mask_size : Vector2 = pin_click_mask_image.get_size()
+	
+	var uv = point/size
+	var point_on_mask = (uv * mask_size).floor()
+	
+	if not Rect2(Vector2.ZERO, mask_size).has_point(point_on_mask):
+		return false
+	
+	var point_r_value = pin_click_mask_image.get_pixelv(point_on_mask).r
+	
+	return point_r_value < 0.5

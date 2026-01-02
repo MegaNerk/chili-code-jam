148240bc26e87755576e68d10dde010ec5632bde
Sandtoasterexe <ojanysantos04@gmail.com>
2026-01-02 14:19:00 -0500

    Added Sound
    I did the thing with the noises and added 6 sound fx

--- Global Systems/Audio/sfx_library.gd
+++ Global Systems/Audio/sfx_library.gd
@@ -4,3 +4,9 @@ class_name SFX_Library
 @export var button_click : AudioStream
 @export var country_hover : AudioStream
 @export var illegal_input : AudioStream
+@export var city_destruction : AudioStream
+@export var building_placement : AudioStream
+@export var kaiju_roar_1 : AudioStream
+@export var kaiju_roar_2 : AudioStream
+@export var kaiju_roar_3 : AudioStream
+@export var click_sound : AudioStream

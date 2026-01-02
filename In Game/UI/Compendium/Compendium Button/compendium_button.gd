5e8b300c0b9edf0e56fb6a81f2e3aeff3b955964
MegaNerk <odrecord@rcn.com>
2026-01-01 21:14:29 -0500

    Add: Compendium shows building costs

--- In Game/UI/Compendium/Compendium Button/compendium_button.gd
+++ In Game/UI/Compendium/Compendium Button/compendium_button.gd
@@ -6,14 +6,32 @@ signal entry_selected(entry_ref : Unit_Res)
 @export var this_entry : Unit_Res:
 	set(value):
 		this_entry = value
-		if $TextureRect and $Label:
-			$TextureRect.texture = this_entry.art
-			$Label.text = this_entry.name
+		sync_with_entry()
+
+@export var food_label : Label
+@export var fear_label : Label
 
 func _ready():
 	if this_entry:
 		$TextureRect.texture = this_entry.art
 		$Label.text = this_entry.name
+		if this_entry is Building_Res:
+			food_label.text = this_entry.food_cost
+			fear_label.text = this_entry.fear_cost
+		else:
+			food_label.visible = false
+			fear_label.visible = false
 
 func on_clicked():
 	emit_signal("entry_selected", this_entry)
+
+func sync_with_entry():
+	if this_entry:
+		$TextureRect.texture = this_entry.art
+		$Label.text = this_entry.name
+		if this_entry is Building_Res:
+			food_label.text = str(this_entry.food_cost)
+			fear_label.text = str(this_entry.fear_cost)
+		else:
+			$PanelContainer.visible = false
+			$PanelContainer2.visible = false

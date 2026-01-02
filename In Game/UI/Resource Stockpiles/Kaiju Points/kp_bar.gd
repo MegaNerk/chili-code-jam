e86c649bd3e953a04b233e53bf1eb60adec8a3d6
MegaNerk <odrecord@rcn.com>
2026-01-02 03:08:40 -0500

    Add: New textures

--- In Game/UI/Resource Stockpiles/Kaiju Points/kp_bar.gd
+++ In Game/UI/Resource Stockpiles/Kaiju Points/kp_bar.gd
@@ -1,7 +1,9 @@
 extends Control
 class_name KPStockpile
 
-@export var kp_count_bar : ProgressBar
+@export var kp_count_bar : TextureProgressBar
+@export var progress : Label
 
 func update_progress(new_progress):
 	kp_count_bar.value = new_progress
+	progress.text = str(new_progress)+"%"

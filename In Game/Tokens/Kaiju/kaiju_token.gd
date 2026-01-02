367ea121133adf26e8655a72d1a3ca28012110a3
Jason clini <33248688+Psyceric@users.noreply.github.com>
2026-01-01 21:10:50 -0500

    Emit signals from Playspace

--- In Game/Tokens/Kaiju/kaiju_token.gd
+++ In Game/Tokens/Kaiju/kaiju_token.gd
@@ -1,13 +1,9 @@
 extends TokenPin
 class_name KaijuToken
 
-@export var nav_agent : NavigationAgent2D
-@export var char_body : CharacterBody2D
-var speed = 100
-var water_speed = 150
-var land_speed = 70
-var target_position = Vector2.ZERO
-var game : Game
-var time_cordinator : TimeCoordinator
-
 var my_kaiju : Kaiju
+var my_info : Kaiju_Res
+
+func _init():
+	
+	pass
